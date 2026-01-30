f56529bae3b471dc154f85cd594ab5d26a2eb4d1
vedantgav-web <vedantgav@gmail.com>
2026-01-31 00:04:26 +0530

    Fix string interpolation in fetch URL

--- Frontend/links.js
+++ Frontend/links.js
@@ -1,6 +1,6 @@
 async function loadLinks() {
   try {
-    const res = await fetch("${API_BASE_URL}/common/links");
+    const res = await fetch(`${API_BASE_URL}/common/links`);
     const links = await res.json();
 
     const container = document.getElementById("linksContainer");
