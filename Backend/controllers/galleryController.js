1e95efc16bfecc5cdb37afc4da0814762d703558
vedantgav-web <vedantgav@gmail.com>
2026-03-23 23:28:12 +0530

    Update galleryController.js

--- Backend/controllers/galleryController.js
+++ Backend/controllers/galleryController.js
@@ -5,7 +5,8 @@ import multer from "multer";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
 
 // Use memory storage for Vercel compatibility
