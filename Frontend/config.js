255e9ce3f4dbc5ba15ab307cfd8b930f50aab4f4
vedantgav-web <vedantgav@gmail.com>
2026-03-23 14:00:55 +0530

    Update config.js

--- Frontend/config.js
+++ Frontend/config.js
@@ -2,6 +2,6 @@ const API_BASE_URL = window.location.hostname === 'localhost' || window.location
     ? 'http://localhost:5000/api' 
     : '/api';
 
-    const IMAGE_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+const IMAGE_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000' 
-    : '';
+    : ''; // Keep this empty for Vercel
